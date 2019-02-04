789089ac49d1b1d73078ecbbfc107897aef2f643
mleugh <2531111+mleugh@users.noreply.github.com>
2019-02-04 14:34:36 +0000

    ga

---
+++ i/ga.js
@@ -0,0 +1,12 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-34787659-1']);
+_gaq.push(['_trackPageview']);
+
+(function () {
+	var s = document.getElementsByTagName('script')[0],
+		ga = document.createElement('script');
+	ga.type = 'text/javascript';
+	ga.async = true;
+	ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	s.parentNode.insertBefore(ga, s);
+}());
